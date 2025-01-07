fd8da278a8abbb85b62d59a1c2f743510c0b2652
g2hy <g2hyeong@gmail.com>
2025-01-07 17:17:52 +0900

    initial settings

---
+++ src/main/java/drivemate/drivemate/DrivemateApplication.java
@@ -0,0 +1,13 @@
+package drivemate.drivemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivemateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivemateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/drivemate/drivemate/DrivemateApplicationTests.java
@@ -0,0 +1,13 @@
+package drivemate.drivemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivemateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
